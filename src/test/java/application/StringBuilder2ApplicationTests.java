a2b5157bb98cc06386c9bf983b58e0c879d7ab97
Delmir Augusto <delmir19.da@gmail.com>
2024-11-15 17:02:41 -0300

    Subindo a aplicacao

---
+++ src/test/java/application/StringBuilder2ApplicationTests.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringBuilder2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
